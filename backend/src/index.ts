226147f16a8507091e173fb425068c51ff5738cb
DeepakMehra53 <dmehra594@gmail.com>
2025-05-15 22:39:42 +0530

    JWT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 import express ,{Request,Response}from "express";
+import jwt from "jsonwebtoken";
+
+
+const JWT_SECRET:string = "Brother>=Stong"
 const app = express();
 const prisma = new PrismaClient()
-
 app.post("/api/v1/user/signup",async(req:Request,res:Response)=>{
     const {username,email,password}= req.body;
-    const signup = await prisma.user.create({
-        data:{
-            username,
-            email,
-            password
-        }
-    })
-    res.status(200).json(signup);
+    try {
+        const signup = await prisma.user.create({
+            data:{
+                username,
+                email,
+                password
+            }
+        })
+        const token = jwt.sign({id:signup.id},JWT_SECRET)
+        res.status(200).json({token,signup})
+    } catch (error) {
+        res.status(500).json({ error: "Signup failed" });
+      }
+   
 })
 
 
-app.post('/api/v1/user/signin',async(req:Request,res:Response)=>{
-    const 
-})
+
 
 app.listen(3000,()=>{
     console.log('Server is running on port no. 3000')
