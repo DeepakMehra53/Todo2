cb66b60bb163c9bf531a7ab541a893144ae07691
DeepakMehra53 <dmehra594@gmail.com>
2025-05-15 22:55:22 +0530

    error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken";
 
 const JWT_SECRET:string = "Brother>=Stong"
 const app = express();
+app.use(express.json())
 const prisma = new PrismaClient()
 app.post("/api/v1/user/signup",async(req:Request,res:Response)=>{
     const {username,email,password}= req.body;
@@ -22,6 +23,31 @@ app.post("/api/v1/user/signup",async(req:Request,res:Response)=>{
         res.status(500).json({ error: "Signup failed" });
       }
    
+})
+
+app.post('/api/v1/user/signin',(req:Request,res:Response)=>{
+    const {username,password}=  req.body
+    try {
+        const signin = prisma.user.findFirst({
+            where:{
+                username,
+                password
+            }
+        })
+
+        if(!signin){
+            res.status(403).json({
+                msg:"Incorrect credential"
+            })
+        }
+
+        const token = jwt.sign({id:signin?.id},JWT_SECRET)
+        return res.json(token)
+    } catch (error) {
+        console.log(error)
+        res.status(403);
+        return res.json({msg:'Invalid'})
+    }
 })
 
 
