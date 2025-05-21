b028c69d350a51bd884afe8927a40b619efff118
DeepakMehra53 <dmehra594@gmail.com>
2025-05-21 21:53:59 +0530

    push

--- Todo-TypeScript/src/components/Auth.tsx
+++ Todo-TypeScript/src/components/Auth.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Auth = () => {
   return (
-    <div className='flex items-center justify-center h-screen'>
+    <div className='flex flex-col items-center justify-center h-screen'>
         <div className='text-3xl font-semibold '>
             Authentification 
         </div>
+        <div className='text-slate-500 '>Create a account? 
+            <Link to={"/signin"}> Login</Link>
+        </div>
     </div>
   )
+}
+
+function Label ({}){
+    
 }
