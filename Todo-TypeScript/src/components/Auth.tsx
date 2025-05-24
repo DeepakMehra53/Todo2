ef9acbe413d1a6ede6c253374f5581505681e9b2
DeepakMehra53 <dmehra594@gmail.com>
2025-05-24 22:50:06 +0530

    Form frontend done

--- Todo-TypeScript/src/components/Auth.tsx
+++ Todo-TypeScript/src/components/Auth.tsx
@@ -1,39 +1,58 @@
-import React, { ChangeEvent, ChangeEventHandler } from 'react'
-import { Link } from 'react-router-dom'
+  import React, { ChangeEvent, ChangeEventHandler, useState } from 'react'
+  import { Link } from 'react-router-dom'
 
-export const Auth = () => {
-  return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-        <div className='text-3xl font-semibold '>
-            Authentification 
-        </div>
-        <div className='text-slate-500 '>Create a account? 
-            <Link to={"/signin"}> Login</Link>
-        </div>
-        <div>
-          <Label  label="Username" placeholder='deepak@gmail.com'/>
-          <Label  label="password" placeholder='D@$fa324*&'/>
-          <button type='button'>Submit</button>
-        </div>
-    </div>
-  )
-}
+  export const Auth = () => {
+    const[username,setUsername]=useState("");
+    const[password,setPassword]=useState("");
 
-
-interface LableControl {
-  type?:string;
-  label:string;
-  placeholder:string;
-  onChange?:ChangeEventHandler<HTMLInputElement>
-  value?:string;
-  
-}
-
-function Label ({type,label,placeholder,onChange,value}:LableControl){
+    const handleUseName: ChangeEventHandler<HTMLInputElement> = (e) => {
+       setUsername(e.target.value);
+      
+    };
     return (
-      <div className='flex flex-col'>
-        <label >{label}</label>
-        <input type={type || "text"} onChange={onChange} value={value} placeholder={placeholder} className='border rounded-md border-slate-400' />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="text-3xl font-semibold ">Authentification</div>
+        <div className="text-slate-500 ">
+          Create a account?
+          <Link to={"/signin"}> Login</Link>
+        </div>
+        <div className="flex flex-col">
+          <Label
+            onChange={handleUseName}
+            label="Username"
+            placeholder="deepak@gmail.com"
+          />
+          <Label
+            onChange={(e) => setPassword(e.target.value)}
+            label="password"
+            placeholder="D@$fa324*&"
+          />
+          <button
+            type="button"
+            className="mt-2 border border-black rounded-lg bg-slate-400 hover:bg-green-300"
+          >
+            Submit
+          </button>
+        </div>
       </div>
-    )
-}
+    );
+  }
+
+
+  interface LableControl {
+    type?:string;
+    label:string;
+    placeholder:string;
+    onChange:ChangeEventHandler<HTMLInputElement>
+    value?:string;
+    
+  }
+
+  function Label ({type,label,placeholder,onChange,value}:LableControl){
+      return (
+        <div className='flex flex-col'>
+          <label >{label}</label>
+          <input type={type || "text"} onChange={onChange} value={value} placeholder={placeholder} className='border rounded-md border-slate-400' />
+        </div>
+      )
+  }
