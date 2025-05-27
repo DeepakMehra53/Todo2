7d970a52eb7e3ff7c15dd839c0981ceb80e4f916
DeepakMehra53 <dmehra594@gmail.com>
2025-05-27 15:23:46 +0530

    Prisma Schema rewrite

--- backend/src/services/todo_services.ts
+++ backend/src/services/todo_services.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient()
 
 export const createTodo = async ({ title, done, description }: { title: string;done:boolean, description: string }) => {
@@ -23,5 +24,26 @@ export const getTodos=async()=>{
         return {status:200,data:{todos}};
     } catch (error) {
         return {status:500,data:{error:`Server error`}}
+    }
+}
+
+export const updateTodo=async(userId:number,todoId:number,data:{title?:string;description?:string;done?:boolean})=>{
+    try {
+        const todo = await prisma.todo.findUnique({where:{id:todoId}})
+        if(!todo){
+            return{status:400,data:{error:"Todo not found"}}
+        }
+        const updatedTodo =await prisma.todo.update({
+            where:{id:todoId},
+            data:{
+                title: data.title,
+                description: data.description,
+                done: data.done
+            }
+        })
+        return { status: 200, data: updatedTodo };
+
+    } catch (error) {
+        return { status: 500, data: { error: 'Server error' } };
     }
 }
