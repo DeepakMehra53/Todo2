ed9cb5db39ee28edb1480aa459f00c17bcf73aca
DeepakMehra53 <dmehra594@gmail.com>
2025-06-09 23:19:42 +0530

    11:19 Controller

--- backend/src/controllers/todoControllers.ts
+++ backend/src/controllers/todoControllers.ts
@@ -1,6 +1,29 @@
 import { Request,Response } from "express";
 import { TodoServices } from '../services/todoServices';
-import { CreateTodo,updateTodo } from "../validators/fromValidators";
+import { updateTodo, createTodo } from '../validators/fromValidators';
+import { boolean } from "yargs";
+import { error } from "console";
+import { title } from "process";
 
-const   todoServices = new  TodoServices();
+
+
+export class TodoController{
+    private todoServices = new TodoServices();
+
+    async  createTodo  (req:Request,res:Response){
+        const parsed=createTodo.safeParse(req.body)
+        if(!parsed.success){res.status(400).json({error:"Invalid input  "})}
+        const { title, description,  } = parsed.data!;
+        const done:boolean =parsed.data!.done ?? false  
+        const userId =(req as any).userId;
+        const todo = await this.todoServices.create(
+            title,
+            description,
+            done,
+            userId
+
+        )
+        return res.json({id:todo.id})
+    }
+}
 
