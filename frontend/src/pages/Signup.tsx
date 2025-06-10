ccf3d6d25f91dfae6c5c37f2d27ed201d37e0a52
DeepakMehra53 <dmehra594@gmail.com>
2025-06-10 23:54:31 +0530

    Signup started

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Quote } from '../components/Quote'
 
 export const Signup = () => {
   return (
-    <div>Signup</div>
-  )
+    <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen">
+      <div>Signup</div>
+      <div>
+        <Quote />
+      </div>
+    </div>
+  );
 }
