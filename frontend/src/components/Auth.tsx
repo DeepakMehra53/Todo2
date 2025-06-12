94035fb0248094e250d7c44d61b58a4802a94f02
DeepakMehra53 <dmehra594@gmail.com>
2025-06-12 22:56:35 +0530

    Signup

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export const Auth = () => {
   return (
-    <div className=''>
+    <div className='w-full'>
+        <div className='flex justify-center h-screen'>
+
         Authentication Page
+        </div>
     </div>
   )
 }
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Quote } from '../components/Quote'
+import { Auth } from '../components/Auth';
 
 export const Signup = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 ">
-      <div>
-        Signup
+      <div className='flex justify-center h-screen'>
+        <Auth/>
         </div>
       <div >
         <Quote />
