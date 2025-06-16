945c486bcd1acfbbf99962690d78556aea5443b7
DeepakMehra53 <dmehra594@gmail.com>
2025-06-16 21:37:35 +0530

    middleware practices

---
+++ backend/index.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+import express from 'express';
+import type { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
+import z from 'zod';
+import { password } from "bun";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const SingUpSchema = z.object({
+    name: z.string().min(1),
+    email: z.string().email(),
+    password: z.string()
+})
+
+const SignInSchema = z.object({
+    email: z.string().email(),
+    password: z.string()
+})
+
+const JWT_SECRET = "sercert2343fs4"
+
+app.use(express.json())
+
+app.use
+
+const authMiddleware =(req:Request,res:Response,next:NextFunction)=>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(402).json({message:"Unauthorized"})
+    }
+    const token = authHeader.split(" ")[1] || " ";
+    try {
+        const decoded = jwt.verify(token,JWT_SECRET)
+    } catch () {
+        
+    }
+}
+
+
+app.post("/api/v1/user/signup",(req:Request,res:Response)=>{
+
+})
+
+app.listen(PORT,()=>{console.log(`Server running on http://localhost:${PORT}`)})
