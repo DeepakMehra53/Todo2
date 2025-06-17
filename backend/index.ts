95550ece58d9b9115bdf8dd1f5ffbfd8edd86031
DeepakMehra53 <dmehra594@gmail.com>
2025-06-17 22:11:44 +0530

    auth middleare

--- backend/index.ts
+++ backend/index.ts
@@ -32,11 +32,14 @@ const authMiddleware =(req:Request,res:Response,next:NextFunction)=>{
     if(!authHeader || !authHeader.startsWith("Bearer ")){
         return res.status(402).json({message:"Unauthorized"})
     }
-    const token = authHeader.split(" ")[1] || " ";
+    const token = authHeader.split(" ")[1] || "";
     try {
-        const decoded = jwt.verify(token,JWT_SECRET)
-    } catch () {
-        
+        const decoded = jwt.verify(token, JWT_SECRET) as { id: string; email: string }
+            (req as any).user = decoded; // attach decoded user info to the request
+        next();
+    } catch (err ) {
+        return res.status(401).json({ message: "Invalid token" });
+
     }
 }
 
