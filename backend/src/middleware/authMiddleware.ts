fa64ae7d5f6cd0a18876939c252d5f188a92db91
DeepakMehra53 <dmehra594@gmail.com>
2025-06-18 21:30:44 +0530

    airport commit

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -4,19 +4,16 @@ import { JWT_SECRET } from "../config/default";
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
-
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-     res.status(401).json({ error: "Unauthorized" });
-     return
+        return res.status(402).json({ message: "Unauthorized" })
     }
-
-    const token = authHeader.split(" ")[1] || ""; 
-
+    const token = authHeader.split(" ")[1] || "";
     try {
-        const user = jwt.verify(token, JWT_SECRET);
-        (req as any).user = user;
+        const decoded = jwt.verify(token, JWT_SECRET) as { id: string; email: string }
+        (req as any).user = decoded; // attach decoded user info to the request
         next();
-    } catch {
-        return res.status(401).json({ error: "Invalid token" });
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid token" });
+
     }
-};
+}
