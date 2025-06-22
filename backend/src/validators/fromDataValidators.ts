06fb09f625fbfb22280791c797a192d86d1dda12
DeepakMehra53 <dmehra594@gmail.com>
2025-06-22 12:04:43 +0530

    signin Schema

--- backend/src/validators/fromDataValidators.ts
+++ backend/src/validators/fromDataValidators.ts
@@ -1,8 +1,14 @@
 import { password } from 'bun'
 import z from 'zod'
+import { email } from 'zod/v4'
 
 export const signupSchema = z.object({
     name:z.string().min(1),
+    email:z.string().email(),
+    password:z.string().min(6)
+})
+
+export const signinSchema = z.object({
     email:z.string().email(),
     password:z.string().min(6)
 })
