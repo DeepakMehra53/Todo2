e6193c3791b87082d1ea39fe06d21ad7b2caea65
DeepakMehra53 <dmehra594@gmail.com>
2025-06-22 12:03:10 +0530

    zod signUp Schema

---
+++ backend/src/validators/fromDataValidators.ts
@@ -0,0 +1,8 @@
+import { password } from 'bun'
+import z from 'zod'
+
+export const signupSchema = z.object({
+    name:z.string().min(1),
+    email:z.string().email(),
+    password:z.string().min(6)
+})
