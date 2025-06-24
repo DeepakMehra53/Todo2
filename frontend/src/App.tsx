25e95a06357ee120de6deacbd107ef6fbb4ee9ec
DeepakMehra53 <dmehra594@gmail.com>
2025-06-25 00:52:38 +0530

    Routing done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 
 export const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/"/>
+        <Route path="signup" element={<Signup/>}/>
+        <Route path="signin" element={<Signin/>}/>
+      </Routes>
+    </Router>
+  );
+};
