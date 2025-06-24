c92076da1224bc4bea7bedc95876d5a12d5263c0
DeepakMehra53 <dmehra594@gmail.com>
2025-06-24 10:05:44 +0530

    userRouter done

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -13,5 +13,14 @@ export const userControllers = {
         return result.success
             ? { status: 200, body: { success: true, token: sign({ id: result.userId }, JWT_SECRET) } }
             : { status: 409, body: { success: false, message: result.message } }
+    },
+    async signin(reqBody: any) {
+        const parsed = signinSchema.safeParse(reqBody)
+        if (!parsed) return { status: 400, body: { msg: "Invalid body" } }
+
+        const result = await userServices.signin(parsed.data);
+        return result.success
+            ? { success: 200, body: { success: true, token: sign({ id: result.userId }, JWT_SECRET) } }
+            : { status: 409, body: { success: false, message: result.message } };
     }
 }
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,27 +1,18 @@
-import { success } from "zod/v4";
 import { userControllers } from "../controllers/userControllers";
-import { userServices } from "../services/userServices";
-import { signinSchema, signupSchema } from "../validators/fromDataValidators";
-import { sign } from "jsonwebtoken";
-import { JWT_SECRET } from "../config/default";
 
-export const userController={
-    async signup (reqBody:any){
-        const parsed = signupSchema.safeParse(reqBody)
-        if(!parsed) return {status:400,body:{msg:"Invalid body"}}
+export const userRoute =async(req:Request): Promise<Response> =>{
+    const url = new URL(req.url)
+    const body= await req.json()
 
-        const result = await userServices.signup(parsed.data);
-        return result.success
-            ?{success:200,body:{success:true,token:sign({id:result.userId},JWT_SECRET)}}
-            :{status:409,body:{success:false,message:result.message}};
-    },
-     async signin(reqBody: any) {
-        const parsed = signinSchema.safeParse(reqBody)
-        if (!parsed) return { status: 400, body: { msg: "Invalid body" } }
+    if (url.pathname === "/api/v1/user/signup" && req.method === "POST"){
+        const result = await userControllers.signup(body);
+        return new Response(JSON.stringify(result.body),{status:result.status})
 
-        const result = await userServices.signin(parsed.data);
-        return result.success
-            ? { success: 200, body: { success: true, token: sign({ id: result.userId }, JWT_SECRET) } }
-            : { status: 409, body: { success: false, message: result.message } };
     }
-}
+    if (url.pathname === "/api/v1/user/signin" && req.method === "POST") {
+        const result = await userControllers.signin(body);
+        return new Response(JSON.stringify(result.body), { status: result.status });
+    }
+
+    return new Response("Route Not Found", { status: 404 });
+}   
